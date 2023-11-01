2d635bc331efb6309b4c73dc9d9e6d54917217e7
Andrew Mitchell <andrew.j.mitchell.247@gmail.com>
2023-11-01 01:12:39 -0400

    cubie edge_multiply and multiply

--- cubie.py
+++ cubie.py
@@ -16,13 +16,15 @@ class CubieCube:
         self.eo = eo[:] if eo else [0] * 12
 
     def corner_multiply(self, other):
-        new_cp = []
-        new_co = []
+        """Multiplies the corners of this cube by another cube, other"""
+        new_cp = [0] * 8
+        new_co = [0] * 8
         co = 0
 
         for c in Corner:
-            new_cp.append(self.cp[other.cp[c]])
-
+            new_cp[c] = self.cp[other.cp[c]]
+            new_co[c] = (self.co[other.cp[c]] + other.co[c]) % 3
+            """
             co1 = self.co[other.cp[c]]
             co2 = other.co[c]
 
@@ -31,11 +33,30 @@ class CubieCube:
                 if co >= 3:
                     co -= 3
 
-            new_co.append(co)
+            new_co[c] = co
+            """
 
         for c in Corner:
             self.cp[c] = new_cp[c]
             self.co[c] = new_co[c]
+
+    def edge_multiply(self, other):
+        """Multiplies the edges of this cube by another cube, other"""
+        new_eo = [0] * 12
+        new_ep = [0] * 12
+
+        for e in Edge:
+            new_eo[e] = (self.eo[other.ep[e]] + other.eo[e]) % 2
+            new_ep[e] = self.ep[other.ep[e]]
+
+        for e in Edge:
+            self.eo[e] = new_eo[e]
+            self.ep[e] = new_ep[e]
+
+    def multiply(self, other):
+        """Multiplies this cube by another cube, other"""
+        self.corner_multiply(other)
+        self.edge_multiply(other)
 
 
 o = CubieCube([Corner.DFR, Corner.UFL, Corner.ULB, Corner.URF, Corner.DRB, Corner.DLF, Corner.DBL, Corner.UBR],
@@ -44,7 +65,7 @@ o = CubieCube([Corner.DFR, Corner.UFL, Corner.ULB, Corner.URF, Corner.DRB, Corne
               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 
 c = CubieCube()
-
-c.corner_multiply(o)
-
 print(c.co, c.cp, c.eo, c.ep)
+c.multiply(o)
+print(c.co, c.cp, c.eo, c.ep)
+
